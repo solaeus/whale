9c77a1f26fb8da21233b8131cd42f2a31e4e6778
Jeff <dev@jeffa.io>
2023-07-24 14:20:04 -0400

    Finish implementing disk::partition #4

--- whale/src/functions/disk.rs
+++ whale/src/functions/disk.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use crate::{BuiltinFunction, FunctionInfo, Result, Value};
 
 pub struct Partition;
@@ -16,6 +18,42 @@ impl BuiltinFunction for Partition {
             .get_value("path")?
             .unwrap_or_default()
             .as_string()?;
+        let label = argument
+            .get_value("label")?
+            .unwrap_or_default()
+            .as_string()?;
+        let name = argument
+            .get_value("name")?
+            .unwrap_or_default()
+            .as_string()?;
+        let filesystem = argument
+            .get_value("filesystem")?
+            .unwrap_or_default()
+            .as_string()?;
+        let mut range = argument
+            .get_value("range")?
+            .unwrap_or_default()
+            .as_tuple()?;
+
+        if range.len() != 2 {
+            return Err(crate::Error::ExpectedFixedLenTuple {
+                expected_len: 2,
+                actual: Value::Tuple(range),
+            });
+        }
+
+        let range_end = range.pop().unwrap_or_default().as_string()?;
+        let range_start = range.pop().unwrap_or_default().as_string()?;
+
+        let script = format!(
+            "sudo parted {path} mklabel {label} mkpart {name} {filesystem} {range_start} {range_end}"
+        );
+
+        Command::new("fish")
+            .arg("-c")
+            .arg(&script)
+            .spawn()?
+            .wait()?;
 
         Ok(Value::String(path))
     }
