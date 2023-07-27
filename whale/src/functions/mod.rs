61a4f270361fbd63df1fbfeb1486277553a9114d
Jeff <dev@jeffa.io>
2023-07-27 00:17:42 -0400

    Write docs

--- whale/src/functions/mod.rs
+++ whale/src/functions/mod.rs
@@ -10,6 +10,10 @@ mod system;
 mod table;
 mod whale;
 
+/// Master list of all internal functions.
+///
+/// This list is used to match identifiers with functions and to provide info
+/// to the shell.
 pub const BUILTIN_FUNCTIONS: [&'static dyn BuiltinFunction; 34] = [
     &command::Bash,
     &command::Fish,
@@ -47,16 +51,23 @@ pub const BUILTIN_FUNCTIONS: [&'static dyn BuiltinFunction; 34] = [
     &whale::RunFile,
 ];
 
+/// Internal whale function with its business logic and all information.
 pub trait BuiltinFunction: Sync + Send {
     fn info(&self) -> FunctionInfo<'static>;
     fn run(&self, argument: &Value) -> Result<Value>;
 }
 
+/// Information needed for each function.
 pub struct FunctionInfo<'a> {
+    /// Text pattern that triggers this function.
     pub identifier: &'a str,
+
+    /// User-facing information about how the function works.
     pub description: &'a str,
 }
 
+/// Searches all functions for a matching identifier and runs the corresponding
+/// function with the given input. Returns the function's output or an error.
 pub fn call_builtin_function(identifier: &str, argument: &Value) -> Result<Value> {
     for function in BUILTIN_FUNCTIONS {
         if identifier == function.info().identifier {
