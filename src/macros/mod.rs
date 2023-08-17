eef48e04f848090b9201176320ef938d53035b73
Jeff <dev@jeffa.io>
2023-08-17 00:38:39 -0400

    Add command macros

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -22,6 +22,7 @@
 use crate::{Result, Value};
 
 mod collections;
+mod command;
 mod data_formats;
 mod disks;
 mod filesystem;
@@ -38,7 +39,12 @@ mod time;
 ///
 /// This list is used to match identifiers with macros and to provide info to
 /// the shell.
-pub const MACRO_LIST: [&'static dyn Macro; 39] = [
+pub const MACRO_LIST: [&'static dyn Macro; 44] = [
+    &command::Bash,
+    &command::Fish,
+    &command::Raw,
+    &command::Sh,
+    &command::Zsh,
     &data_formats::FromJson,
     &filesystem::Append,
     &filesystem::CreateDir,
@@ -515,112 +521,6 @@ pub struct MacroInfo<'a> {
 //         };
 
 //         Ok(Value::Integer(len as i64))
-//     }
-// }
-
-// pub struct Sh;
-
-// impl Macro for Sh {
-//     fn info(&self) -> MacroInfo<'static> {
-//         MacroInfo {
-//             identifier: "sh",
-//             description: "Pass input to the Bourne Shell.",
-//         }
-//     }
-
-//     fn run(&self, argument: &Value) -> Result<Value> {
-//         let argument = argument.as_string()?;
-
-//         Command::new("sh").arg("-c").arg(argument).spawn()?.wait()?;
-
-//         Ok(Value::Empty)
-//     }
-// }
-
-// pub struct Bash;
-
-// impl Macro for Bash {
-//     fn info(&self) -> MacroInfo<'static> {
-//         MacroInfo {
-//             identifier: "bash",
-//             description: "Pass input to the Bourne Again Shell.",
-//         }
-//     }
-
-//     fn run(&self, argument: &Value) -> Result<Value> {
-//         let argument = argument.as_string()?;
-
-//         Command::new("bash")
-//             .arg("-c")
-//             .arg(argument)
-//             .spawn()?
-//             .wait()?;
-
-//         Ok(Value::Empty)
-//     }
-// }
-// pub struct Fish;
-
-// impl Macro for Fish {
-//     fn info(&self) -> MacroInfo<'static> {
-//         MacroInfo {
-//             identifier: "fish",
-//             description: "Pass input to the fish shell.",
-//         }
-//     }
-
-//     fn run(&self, argument: &Value) -> Result<Value> {
-//         let argument = argument.as_string()?;
-
-//         Command::new("fish")
-//             .arg("-c")
-//             .arg(argument)
-//             .spawn()?
-//             .wait()?;
-
-//         Ok(Value::Empty)
-//     }
-// }
-
-// pub struct Zsh;
-
-// impl Macro for Zsh {
-//     fn info(&self) -> MacroInfo<'static> {
-//         MacroInfo {
-//             identifier: "zsh",
-//             description: "Pass input to the Z shell.",
-//         }
-//     }
-
-//     fn run(&self, argument: &Value) -> Result<Value> {
-//         let argument = argument.as_string()?;
-
-//         Command::new("zsh")
-//             .arg("-c")
-//             .arg(argument)
-//             .spawn()?
-//             .wait()?;
-
-//         Ok(Value::Empty)
-//     }
-// }
-
-// pub struct Raw;
-
-// impl Macro for Raw {
-//     fn info(&self) -> MacroInfo<'static> {
-//         MacroInfo {
-//             identifier: "raw",
-//             description: "Run input as a command without a shell",
-//         }
-//     }
-
-//     fn run(&self, argument: &Value) -> Result<Value> {
-//         let argument = argument.as_string()?;
-
-//         Command::new(argument).spawn()?.wait()?;
-
-//         Ok(Value::Empty)
 //     }
 // }
 
