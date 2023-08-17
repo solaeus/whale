61159d7da866b0c6800b1bf9e743bdd22356a25e
Jeff <dev@jeffa.io>
2023-08-16 20:23:42 -0400

    Improve argument parsing for plot

--- src/macros/gui.rs
+++ src/macros/gui.rs
@@ -6,7 +6,7 @@ use eframe::{
     run_native, NativeOptions,
 };
 
-use crate::{Macro, MacroInfo, Result, Value};
+use crate::{Error, Macro, MacroInfo, Result, Value};
 
 pub struct Plot;
 
@@ -20,12 +20,23 @@ impl Macro for Plot {
     }
 
     fn run(&self, argument: &Value) -> Result<Value> {
-        let argument = argument.as_list()?.clone();
+        let argument = argument.as_list()?;
+        let mut floats = Vec::new();
+
+        for value in argument {
+            if let Ok(float) = value.as_float() {
+                floats.push(float);
+            } else if let Ok(integer) = value.as_int() {
+                floats.push(integer as f64);
+            } else {
+                return Err(Error::expected_number(value.clone()));
+            }
+        }
 
         run_native(
             "Whale Gui",
             NativeOptions::default(),
-            Box::new(|_cc| Box::new(PlotGui::new(argument))),
+            Box::new(|_cc| Box::new(PlotGui::new(floats))),
         )
         .unwrap();
 
@@ -34,11 +45,11 @@ impl Macro for Plot {
 }
 
 struct PlotGui {
-    data: Vec<Value>,
+    data: Vec<f64>,
 }
 
 impl PlotGui {
-    fn new(data: Vec<Value>) -> Self {
+    fn new(data: Vec<f64>) -> Self {
         Self { data }
     }
 }
@@ -51,7 +62,7 @@ impl eframe::App for PlotGui {
                     .data
                     .iter()
                     .enumerate()
-                    .map(|(index, value)| [index as f64, value.as_float().unwrap()])
+                    .map(|(index, value)| [index as f64, *value])
                     .collect::<PlotPoints>();
                 let line = Line::new(points);
                 plot_ui.line(line);
